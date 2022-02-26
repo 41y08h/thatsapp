e1fa7691664cf06138148af87825804d77e331fa
Piyush <piyush41y08h@gmail.com>
2022-02-26 19:52:37 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:thatsapp/screens/home_screen.dart';
+import 'package:thatsapp/screens/login_screen.dart';
+import 'package:thatsapp/screens/signup_screen.dart';
+
+void main() async {
+  // Check if user is already logged in
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token');
+  bool isAuthenticated = token != null;
+
+  runApp(
+    MaterialApp(
+      title: "ThatsApp",
+      initialRoute: isAuthenticated ? "home" : "signup",
+      routes: {
+        "login": (context) => const LoginScreen(),
+        "signup": (context) => const SignupScreen(),
+        "home": (context) => const HomeScreen(),
+      },
+    ),
+  );
+}
